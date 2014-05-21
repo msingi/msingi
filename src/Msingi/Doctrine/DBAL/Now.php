b07c2e287e75686526479c07b5e05b402d98a2df
Andrey Ovcharov <andrew.ovcharov@gmail.com>
2014-05-21 17:21:25 +0400

    Added NOW() function

---
+++ src/Msingi/Doctrine/DBAL/Now.php
@@ -0,0 +1,38 @@
+<?php
+namespace Msingi\Doctrine\DBAL;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+
+/**
+ * Implementation of MySQL function NOW()
+ *
+ * @package Msingi\Doctrine\DBAL
+ */
+class Now extends FunctionNode
+{
+    protected $expr;
+
+    /**
+     * @param \Doctrine\ORM\Query\SqlWalker $sqlWalker
+     *
+     * @return string
+     */
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return 'NOW()';
+    }
+
+    /**
+     * @param \Doctrine\ORM\Query\Parser $parser
+     *
+     * @return void
+     */
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
