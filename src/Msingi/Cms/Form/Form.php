d8a6d4c7005246705fb6e0a3f78c293ad23aa97f
Andrey Ovcharov <andrew.ovcharov@gmail.com>
2014-07-03 19:47:37 +0400

    no message

--- src/Msingi/Cms/Form/Form.php
+++ src/Msingi/Cms/Form/Form.php
@@ -2,16 +2,54 @@
 
 namespace Msingi\Cms\Form;
 
-class Form extends \Zend\Form\Form
+use Zend\InputFilter\InputFilterProviderInterface;
+
+/**
+ * Class Form
+ * @package Msingi\Cms\Form
+ */
+class Form extends \Zend\Form\Form implements InputFilterProviderInterface
 {
+    /** @var array */
+    protected $inputFilterSpecification = array();
+
+    /**
+     * Should return an array specification compatible with
+     * {@link Zend\InputFilter\Factory::createInputFilter()}.
+     *
+     * @return array
+     */
+    public function getInputFilterSpecification()
+    {
+        return $this->inputFilterSpecification;
+    }
+
     /**
      * Placeholder function for gettext translation collector
      *
      * @param $text
      * @return mixed
      */
-    public function _($text)
+    protected function _($text)
     {
         return $text;
+    }
+
+    /**
+     * @return $this
+     */
+    protected function addCsrfInput()
+    {
+        $this->add(array(
+            'type' => 'Csrf',
+            'name' => 'csrf',
+            'options' => array(
+                'csrf_options' => array(
+                    'timeout' => 600,
+                )
+            ),
+        ));
+
+        return $this;
     }
 }
